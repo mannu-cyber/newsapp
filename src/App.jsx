8a32ad4947a69d56dd6a915688c96e4fc92a3e84
cyber-mannu <sahanimannu844@gmail.com>
2024-03-31 18:22:10 +0530

    name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
   const [news, setNews] = useState([]);
-  const loadNews = async () => {
-    const response = await axios.get(
-      "https://newsapi.org/v2/everything?q=keyword&apiKey=51a66b41f7494e2a99e7c10b6e6b1f12"
-    );
+  const [error, setError] = useState(null);
 
-    setNews(response.data.articles);
+  const loadNews = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/everything?q=keyword&apiKey=51a66b41f7494e2a99e7c10b6e6b1f12"
+      );
+      setNews(response.data.articles);
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   useEffect(() => {
     loadNews();
   }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="flex flex-wrap justify-center gap-5">
       {news.map((newsArticles, index) => {
@@ -26,7 +35,7 @@ function App() {
           description,
           url,
           urlToImage,
-          publishedat,
+          publishedAt,
           content,
         } = newsArticles;
 
@@ -37,11 +46,12 @@ function App() {
             <p>{description}</p>
             <h3>{author}</h3>
             <p>{content}</p>
-            <p> {publishedat}</p>
+            <p> {publishedAt}</p>
             <a
               href={url}
               className="px-8 py-2 bg-pink-600 inline-block mt-5 rounded-2xl"
               target="_blank"
+              rel="noopener noreferrer"
             >
               Read More
             </a>
